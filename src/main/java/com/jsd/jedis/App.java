7c8396fd19f0635220c9bb502dc158e3c4ca1969
JayDBC <jay.dastur@redis.com>
2024-08-20 11:08:20 -0400

    2024-08-20 1108

--- src/main/java/com/jsd/jedis/App.java
+++ src/main/java/com/jsd/jedis/App.java
@@ -1,13 +1,10 @@
 package com.jsd.jedis;
 
 
-import java.security.GeneralSecurityException;
-import java.security.cert.X509Certificate;
+import java.io.FileInputStream;
+import java.util.Properties;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+
 
 
 import com.jsd.utils.*;
@@ -23,69 +20,31 @@ import com.jsd.utils.*;
  */
 public class App {
 
-    static void disableCertValidaton() {
 
-        
-        javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
-                new javax.net.ssl.HostnameVerifier() {
-                    public boolean verify(String hostname,
-                            javax.net.ssl.SSLSession sslSession) {
-                            return true; // or return true
-                    }
-                });
-        
-
-        // Create a trust manager that does not validate certificate chains
-        TrustManager[] trustAllCerts = new TrustManager[] {
-
-                new X509TrustManager() {
-
-                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                        return new X509Certificate[0];
-                    }
-
-                    public void checkClientTrusted(
-                            java.security.cert.X509Certificate[] certs, String authType) {
-                    }
-
-                    public void checkServerTrusted(
-                            java.security.cert.X509Certificate[] certs, String authType) {
-                    }
-                }
-        };
-
-        try {
-            SSLContext sc = SSLContext.getInstance("SSL");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-        } catch (GeneralSecurityException e) {
-        }
-    }
 
     public static void main(String[] args) throws Exception {
 
-        //disable check for self signed certs when using TLS
-        disableCertValidaton();
+        Properties config = new Properties();
+
+        String configFile = "./config-load.properties";
+
+        config.load(new FileInputStream(configFile));
+        RedisDataLoader redisDataLoader = new RedisDataLoader(configFile);
+
+        String filePath =  config.getProperty("data.file");
+        String keyPrefix = config.getProperty("data.key.prefix");
 
 
-        RedisDataLoader redisDataLoader = new RedisDataLoader("./config.properties");
-
-        String filePath = "C:/Users/Jay Datsur/OneDrive/Tech/Redis/DataSets";
-
-        String fileName = "";
-        fileName = "support_tickets_demo.csv";
-        //fileName = "subscriptions.csv";
-
-        CSVScanner scanner = new CSVScanner(filePath + "/" +  fileName,",",false);
+        CSVScanner scanner = new CSVScanner(filePath,",",false);
 
         //load into hash
         //redisDataLoader.loadHash("hash:support:tickets:", "TicketID", scanner);
 
         //load into JSON
-        String keyType = "random"; // header or random
+        String keyType = "header"; // header or random
 
-        //redisDataLoader.loadJSON("healthcare:subscriptions:", keyType, "SubscriberUniqueID", "DependantUniqueID", "dependants", subscriptions, 0);
-        redisDataLoader.loadJSON("support:tickets:", keyType, "TicketID", "NA", "NA", scanner, 0);
+        redisDataLoader.loadJSON(keyPrefix, keyType, "SubscriberUniqueID", "DependantUniqueID", "dependants", scanner, 0);
+        //redisDataLoader.loadJSON(keyPrefix, keyType, "TicketID", "NA", "NA", scanner, 0);
 
         redisDataLoader.close();
 
