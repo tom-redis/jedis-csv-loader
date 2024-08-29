fa5cb9570c5ec0316d3f74c536e6132c29569393
JayDBC <jay.dastur@redis.com>
2024-08-29 15:29:13 -0400

    2024-08-29 15:16

--- src/main/java/com/jsd/jedis/App.java
+++ src/main/java/com/jsd/jedis/App.java
@@ -3,9 +3,7 @@ package com.jsd.jedis;
 
 import java.io.FileInputStream;
 import java.util.Properties;
-
-
-
+import java.util.Scanner;
 
 import com.jsd.utils.*;
 
@@ -26,7 +24,17 @@ public class App {
 
         Properties config = new Properties();
 
-        String configFile = "./config-load.properties";
+        Scanner s = new Scanner(System.in);
+
+        // set the config file
+        String configFile = "./config.properties";
+
+        System.out.print("\nEnter the config file path (Defaults to ./config.properties): ");
+        String configFile1  = s.nextLine();
+
+        if(!"".equals(configFile1)) {
+            configFile = configFile1;
+        }
 
         config.load(new FileInputStream(configFile));
         RedisDataLoader redisDataLoader = new RedisDataLoader(configFile);
@@ -37,16 +45,24 @@ public class App {
 
         CSVScanner scanner = new CSVScanner(filePath,",",false);
 
-        //load into hash
-        //redisDataLoader.loadHash("hash:support:tickets:", "TicketID", scanner);
+
 
         //load into JSON
         String keyType = "header"; // header or random
 
-        redisDataLoader.loadJSON(keyPrefix, keyType, "SubscriberUniqueID", "DependantUniqueID", "dependants", scanner, 0);
-        //redisDataLoader.loadJSON(keyPrefix, keyType, "TicketID", "NA", "NA", scanner, 0);
+        String headerID = config.getProperty("data.header.field");
+        String detailID = config.getProperty("data.detail.field");
+        String detailName = config.getProperty("data.detail.attr.name");
+
+        //load into hash
+        //redisDataLoader.loadHash(keyPrefix, headerID, scanner);        
+
+        redisDataLoader.loadJSON(keyPrefix, keyType, headerID, detailID, detailName, scanner, 0);
+
 
         redisDataLoader.close();
+
+        s.close();
 
     }
     
